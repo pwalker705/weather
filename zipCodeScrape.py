7b06468e651a37b36b63ff1f6025e34e4966ae3c
Paul Office <pwalker705@gmail.com>
2019-05-01 12:13:39 -0400

    Updated scraper to loop over all cities and write out zip codes as csv. Also fix errors in state abbreviations

--- zipCodeScrape.py
+++ zipCodeScrape.py
@@ -2,24 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np 
-from tabulate import tabulate
+import csv
 
 zips = []
-page = requests.get("https://www.zip-codes.com/city/ga-atlanta.asp")
 
-soup = BeautifulSoup(page.content, 'html.parser')
+with open('cities.csv', mode = 'r') as csv_file:
+	csv_reader = csv.DictReader(csv_file)	
+	for row in csv_reader:
+		link = ("https://www.zip-codes.com/city/" + (f'{row["STATE"]}') + '-' + str.replace((f'{row["CITY"]}'), " ", "-") + ".asp")
+			
+		city = (f'{row["CITY"]}' + ',' + f' {row["STATE"]}')
+		page = requests.get(link)
+		soup = BeautifulSoup(page.content, 'html.parser')
+		zip = soup.find(id = 'tblZIP')
+		i = 1
+		for row in zip.find_all('tr'):
+			col = row.find_all('td')
+			if i > 1:
+				zips.append((city, col[0].text.strip()[-5:], col[1].text.strip(), col[2].text.strip(), col[3].text.strip()))
+			i += 1	
+	df = pd.DataFrame(zips)
+	df.columns = ['City', 'ZipCode', 'Type', 'County', 'Population']
+	zipCodes = df.to_csv(r'zipCode.csv', index =False, header = True)	
 
-zip = soup.find(id = 'tblZIP')
-
-#header = zip.find_all('tr')
-#print(header[2].text)
-
-for row in zip.find_all('tr'):
-	col = row.find_all('td')
-	zips.append((col[0].text.strip(), col[1].text.strip(), col[2].text.strip(), col[3].text.strip(), col[4].text.strip()))
-	#print(col[2].text.strip())
-
-#print(zips)
-df = pd.DataFrame(zips)
-
-print(df.head(10))
