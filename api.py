3231749087ab3ba736f858a37234096b54fe4b5e
pwalker705 <pwalker705@gmail.com>
2019-05-05 09:21:53 -0400

    fix git and push work from yesterday. Trying out new approach to api

--- api.py
+++ api.py
@@ -1,20 +1,14 @@
 import os
 import requests
-from noaa_api_v2 import NOAAData
+#from noaa_api_v2 import NOAAData
+from noaa_sdk import noaa
 
 key = os.environ.get('WEATHER_API_TOKEN')
 
-data = NOAAData(key)
-
-#categories = data.data_categories(locationid='FIPS:37', sortfield='name')
-
-#for i in categories:
-#    print(i)
-#headers = {'token': key}
-#r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?locationid=FIPS:13', headers= headers)
-#weather_data = data.fetch_data(datasetid='GHCND', locationid='ZIP:26501', startdate='2019-04-28', enddate='2019-04-28', limit=10
-
-#print(categories)
+n = noaa.NOAA()
+#print(n.points_forecast(40.7314, -73.8656, hourly=False))
+hourly = n.points_forecast(39.618500, -79.947453, hourly=True)
+print(hourly)
 
 
 
